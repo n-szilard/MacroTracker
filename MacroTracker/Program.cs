faf0659ec0237875dd88a8722c3130eba1fe5751
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-01-22 11:18:41 +0100

    Metódusok készítése

--- MacroTracker/Program.cs
+++ MacroTracker/Program.cs
@@ -2,9 +2,40 @@
 
 internal class Program
 {
+
     private static void Main(string[] args)
     {
-        List<Kaja> kajaLista = new List<Kaja>();
-        Beolvaso.Beolvasas();
+        //List<Kaja> kajaLista = new List<Kaja>();
+        //Beolvaso.Beolvasas();
+        /*AdatokKiirasa();            */
+        Etelfelvetel();
+
+
     }
+
+    private static void Etelfelvetel()
+    {
+        Console.WriteLine("----Kaja felvétel----");
+        Console.WriteLine("Add meg a kaja nevét!");
+        string felvettnev=Console.ReadLine();
+        Console.WriteLine("Add meg a kaja grammját!");
+        string felvettgram = Console.ReadLine();
+        Console.WriteLine("Add meg a kaja kaloriáját!");
+        string felvettkaloria = Console.ReadLine();
+        Console.WriteLine("Add meg a kaja fehérjéjét!");
+        string felvettfeherje = Console.ReadLine();
+        Console.WriteLine("Add meg a kaja zsírját!");
+        string felvettzsir = Console.ReadLine();
+
+    }
+    
+
+    /*private static void AdatokKiirasa()
+    {
+        foreach(dolog in dolog)
+        {
+            Console.WriteLine(adat);
+        }
+    }
+    */
 }
