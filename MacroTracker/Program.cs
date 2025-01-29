d3483d7488b6add016d71beff0f3f8d0b23ace1c
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-01-29 11:24:51 +0100

    Étel felvétel és magas kalóriás feladat elkészítése

--- MacroTracker/Program.cs
+++ MacroTracker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MacroTracker;
+using System.Reflection.Emit;
 
 internal class Program
 {
@@ -7,24 +8,55 @@ internal class Program
     {
         List<Kaja> kajaLista = Beolvaso.Beolvasas();
         AdatokKiirasa(kajaLista);
-        //Etelfelvetel();
+        Sortores();
 
+        Etelfelvetel();
+        Sortores();
+        MagasKaloriaKajak(kajaLista);
+        Sortores();
 
+    }
+
+    private static void MagasKaloriaKajak(List<Kaja> lista)
+    {
+        Console.WriteLine("Ételek melyekben 800-nál több kalória van");
+        Sortores();
+        foreach (var item in lista)
+        {
+            if (item.Kaloria >= 800)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+
+    private static void Sortores()
+    {
+        Console.WriteLine("------------------------------------------");
     }
 
     private static void Etelfelvetel()
     {
         Console.WriteLine("----Kaja felvétel----");
-        Console.WriteLine("Add meg a kaja nevét!");
-        string felvettnev=Console.ReadLine();
-        Console.WriteLine("Add meg a kaja grammját!");
-        string felvettgram = Console.ReadLine();
-        Console.WriteLine("Add meg a kaja kaloriáját!");
-        string felvettkaloria = Console.ReadLine();
-        Console.WriteLine("Add meg a kaja fehérjéjét!");
-        string felvettfeherje = Console.ReadLine();
-        Console.WriteLine("Add meg a kaja zsírját!");
-        string felvettzsir = Console.ReadLine();
+        Console.WriteLine("Szeretnél ételt felvenni? (igen/nem)");
+
+        string felvetelvagynem =Console.ReadLine();
+        if (felvetelvagynem=="igen")
+        {
+            Console.WriteLine("Add meg a kaja nevét!");
+            string felvettnev = Console.ReadLine();
+            Console.WriteLine("Add meg a kaja grammját!");
+            string felvettgram = Console.ReadLine();
+            Console.WriteLine("Add meg a kaja kaloriáját!");
+            string felvettkaloria = Console.ReadLine();
+            Console.WriteLine("Add meg a kaja fehérjéjét!");
+            string felvettfeherje = Console.ReadLine();
+            Console.WriteLine("Add meg a kaja zsírját!");
+            string felvettzsir = Console.ReadLine();
+        }
+        Console.WriteLine("----Kaja felvétel vége----");
+        
+        
 
     }
 
